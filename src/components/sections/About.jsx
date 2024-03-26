e42790ae437da3549943299a27ddb030b69606d3
BalbianoLuciano <balbiano06@gmail.com>
2024-03-26 19:00:59 -0300

    abuot me section

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../utils/118n";
+import { useTranslation } from "react-i18next";
+import "../../styles/global.css";
+
+const About = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="">
+      <div className="flex flex-row relative">
+        <div className="flex w-5/6 flex-col justify-end px-10 py-14 h-96 bg-05 max-h-[300px] max-w-[800px]">
+          <div className="mt-24 text-l font-normal roles-border-half text-right text-00 max-md:mt-10 max-md:max-w-full">
+            {t("description")}
+          </div>
+        </div>
+        <div className="text-6xl absolute -right-16 bottom-28 sections-border-half font-medium text-04 tracking-[.10em] -rotate-90">
+          {t("about me")}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
