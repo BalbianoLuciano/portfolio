a7256a8a82fec210eff0b83f0246d59a5068c33b
BalbianoLuciano <balbiano06@gmail.com>
2024-05-02 09:50:41 -0300

    Mobile version completed

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
 
   return (
     <div className="" id="about">
-      <div className="flex flex-row relative">
+      <div className="hidden lg:flex flex-row relative">
         <div className="flex w-5/6 flex-col justify-end px-5 xl:px-10 py-7 xl:py-14 h-96 bg-05 max-h-[200px] xl:max-h-[300px] max-w-[800px] 2xl:max-w-[1200px]">
           <div className="text-sm xl:text-lg font-normal roles-border-half text-right text-00">
             {t("description")}
@@ -16,6 +16,14 @@ const About = () => {
         </div>
         <div className="text-4xl -right-[35px] bottom-16 xl:text-5xl xl:-right-10 xl:bottom-20 absolute sections-border-half font-medium text-04 tracking-[.10em] -rotate-90">
           {t("about me")}
+        </div>
+      </div>
+      <div className="bg-04 flex flex-col justify-between p-5 w-full h-[250px] lg:hidden">
+        <div className="text-5xl font-extrabold tracking-[1rem] text-01">
+          {t("about me")}
+        </div>
+        <div className="text-xs font-normal text-right text-01">
+          {t("description")}
         </div>
       </div>
     </div>
