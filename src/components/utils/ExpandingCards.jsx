fd68f0e46591bf56fab4db853956a93be7fc92e6
BalbianoLuciano <balbiano06@gmail.com>
2024-05-02 12:42:46 -0300

    conditional changed

--- src/components/utils/ExpandingCards.jsx
+++ src/components/utils/ExpandingCards.jsx
@@ -15,20 +15,18 @@ const ExpandingCards = () => {
           style={{ backgroundImage: `url(${card.imageUrl})` }}
           onClick={() => setActiveIndex(card.id)}
         >
-          {card.id === activeIndex && (
-            <div className="techBadges">
+          
+            <div className={`techBadges ${card.id === activeIndex ? "" : "mobile-hidden"}`}>
               {card.technologies.map((tech) => (
                 <span key={tech} className="techBadge">
                   {tech}
                 </span>
               ))}
             </div>
-          )}
           <div className="infoContainer">
             <h3 className="text-xs xl:text-sm 2xl:text-base cards-border-half">{t(card.title)}</h3>
-            {card.id === activeIndex && (
               <a
-                className="button"
+                className={`button ${card.id === activeIndex ? "" : "mobile-hidden"}`}
                 rel="noopener noreferrer"
                 href={card.link}
                 target="_blank"
@@ -62,7 +60,6 @@ const ExpandingCards = () => {
                 </span>
                 {t("link to")}
               </a>
-            )}
           </div>
         </div>
       ))}
