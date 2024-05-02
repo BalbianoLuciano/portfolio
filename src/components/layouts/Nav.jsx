63047d664a04d2d41a3a8fc575906507980832f7
BalbianoLuciano <balbiano06@gmail.com>
2024-05-02 09:50:32 -0300

    Mobile versión uncompleted

--- src/components/layouts/Nav.jsx
+++ src/components/layouts/Nav.jsx
@@ -1,25 +1,32 @@
-import React from "react";
-import "../utils/118n";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import "../../styles/global.css";
 
 const Nav = () => {
   const { t } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
     <header className="w-full mt-10">
-      <nav className="flex flex-row xl:py-4">
-        <ul className="flex w-auto justify-evenly uppercase">
-          <li className="mr-4 li-border-half">
+      <nav className="flex flex-row lg:py-4">
+        {/* Botón del menú de hamburguesa solo visible en pantallas menores a lg */}
+        <button className="hidden lg:hidden md:inline-block sm:inline-block xs:inline-block" onClick={toggleMenu}>
+          <i className="bi bi-list"></i>
+        </button>
+        {/* Menú original que se oculta en tamaños menores a lg */}
+        <ul className={`${isOpen ? "flex" : "hidden"} flex-col lg:flex lg:flex-row w-auto justify-evenly uppercase lg:uppercase`}>
+          <li className="mr-4 lg:mr-4 li-border-half">
             <a href="#about">{t("about")}</a>
           </li>
-          <li className="mx-4 li-border-half">
+          <li className="mx-4 lg:mx-4 li-border-half">
             <a href="#works">{t("works")}</a>
           </li>
-          <li className="mx-4 li-border-half">
+          <li className="mx-4 lg:mx-4 li-border-half">
             <a href="#skills">{t("skills")}</a>
           </li>
-          <li className="mx-4 li-border-half">
+          <li className="mx-4 lg:mx-4 li-border-half">
             <a href="#education">{t("education")}</a>
           </li>
         </ul>
--- src/components/sections/Works.jsx
+++ src/components/sections/Works.jsx
@@ -9,12 +9,20 @@ const Works = () => {
 
   return (
     <div className="" id="works">
-      <div className="flex flex-row relative">
+      <div className="hidden lg:flex flex-row relative">
         <div className="flex w-5/6 flex-col bg-05 h-96 max-h-[300px] xl:max-h-[440px] max-w-[800px] 2xl:max-w-[1200px]">
-            <ExpandingCards />
+          <ExpandingCards />
         </div>
         <div className="text-4xl -right-1 bottom-8 absolute xl:text-5xl xl:-right-0 xl:bottom-10 sections-border-half font-medium text-04 tracking-[.10em] -rotate-90">
           {t("works title")}
+        </div>
+      </div>
+      <div className="bg-01 flex flex-col justify-between p-5 w-full lg:hidden">
+        <div className="text-5xl font-extrabold pb-5 text-04 tracking-[.10em]">
+          {t("works title")}
+        </div>
+        <div className="bg-05">
+          <ExpandingCards />
         </div>
       </div>
     </div>
